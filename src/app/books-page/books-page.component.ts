a590ee846795341ecd2f3abf7cd09f8ffadcdae2
aa602 <123@gmail.com>
2022-11-29 20:34:33 +0800

    利用資料單向繫結綁定頁面與邏輯

--- src/app/books-page/books-page.component.ts
+++ src/app/books-page/books-page.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./books-page.component.css']
 })
 export class BooksPageComponent {
+  bookName: string = 'Book A';
+  author = '張三';
 
+  onSearch(): void {
+    console.log('click search button');
+  }
+
+  onAdd(): void {
+    console.log('click add button');
+  }
+
+  onEdit(index: number): void {
+    console.log(`click edit button. index = ${index}`);
+  }
+
+  onDelete(index: number): void {
+    console.log(`click delete button. index = ${index}`);
+  }
 }
