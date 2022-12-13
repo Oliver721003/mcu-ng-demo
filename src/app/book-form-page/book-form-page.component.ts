c9d08420bf5df2cfc2cff4bbfade6710ea4a0c59
aa602 <123@gmail.com>
2022-12-13 19:51:43 +0800

    利用 ng form 實作書籍表單

--- src/app/book-form-page/book-form-page.component.ts
+++ src/app/book-form-page/book-form-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Book } from '../model/book';
 import { BookService } from '../services/book.service';
@@ -9,16 +10,12 @@ import { BookService } from '../services/book.service';
   styleUrls: ['./book-form-page.component.css']
 })
 export class BookFormPageComponent {
-  bookName = 'Book A';
-
-  author = '王五';
-
-  company = '銘傳';
-
   constructor(private router: Router, private bookService: BookService) {}
 
-  onAdd(): void {
-    const book = new Book(this.bookName, this.author, this.company);
+  onAdd(form: NgForm): void {
+    const { name, author, company} = form.value;
+    // const book = new Book(form.value);
+    const book = new Book(name, author, company);
     this.bookService.add(book);
     this.router.navigate(['books']);
   }
